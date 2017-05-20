c7c83ab9b9b69bfba1474ceac94a135207184179
maxiaowen <332168460@qq.com>
2017-05-20 10:21:37 +0800

    本地视频列表

---
+++ app/src/main/java/com/atguigu/mobileplayer0224/activity/SystemVideoPlayerActivity.java
@@ -0,0 +1,66 @@
+package com.atguigu.mobileplayer0224.activity;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import com.atguigu.mobileplayer0224.R;
+
+public class SystemVideoPlayerActivity extends AppCompatActivity {
+
+    private VideoView vv;
+    private Uri uri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_system_video_player);
+        vv = (VideoView)findViewById(R.id.vv);
+
+        //得到播放地址
+        uri = getIntent().getData();
+
+        //设置播放器三个监听：播放准备好的监听，播放完成的监听，播放出错的监听
+
+        //底层准备播放完成的时候回调
+        vv.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+
+                //开始播放
+                vv.start();
+            }
+        });
+
+        //设置监听播放出错
+        vv.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+            @Override
+            public boolean onError(MediaPlayer mp, int what, int extra) {
+                Toast.makeText(SystemVideoPlayerActivity.this, "播放出错了", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        //设置监听播放完成
+        vv.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                Toast.makeText(SystemVideoPlayerActivity.this, "播放完成", Toast.LENGTH_SHORT).show();
+                //退出当前页面
+                finish();
+            }
+        });
+
+        //设置播放地址
+        vv.setVideoURI(uri);
+
+        //设置控制面板
+        vv.setMediaController(new MediaController(this));
+
+
+    }
+}
