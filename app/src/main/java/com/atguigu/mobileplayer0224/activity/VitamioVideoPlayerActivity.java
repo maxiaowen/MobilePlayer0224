41ef4df5566e5339e3a3801120a5fe56c981d172
maxiaowen <332168460@qq.com>
2017-05-24 10:20:28 +0800

    手指在屏幕上下滑动，实现亮度的变化

--- app/src/main/java/com/atguigu/mobileplayer0224/activity/VitamioVideoPlayerActivity.java
+++ app/src/main/java/com/atguigu/mobileplayer0224/activity/VitamioVideoPlayerActivity.java
@@ -18,6 +18,7 @@ import android.view.GestureDetector;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -464,10 +465,27 @@ public class VitamioVideoPlayerActivity extends AppCompatActivity implements Vie
 
     //记录坐标
     private float dowY;
+    private float dowX;
     //滑动的初始声音
     private int mVol;
     //滑动的最大区域
     private float touchRang;
+
+    /*
+ * 设置屏幕亮度
+ * 0 最暗
+ * 1 最亮
+ */
+    public void setBrightness(float brightness) {
+        WindowManager.LayoutParams lp = getWindow().getAttributes();
+        lp.screenBrightness = lp.screenBrightness + brightness / 255.0f;
+        if (lp.screenBrightness > 1) {
+            lp.screenBrightness = 1;
+        } else if (lp.screenBrightness < 0.1) {
+            lp.screenBrightness = (float) 0.1;
+        }
+        getWindow().setAttributes(lp);
+    }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
@@ -477,6 +495,7 @@ public class VitamioVideoPlayerActivity extends AppCompatActivity implements Vie
             case MotionEvent.ACTION_DOWN:
                 //1.记录相关参数
                 dowY = event.getY();
+                dowX = event.getX();
                 mVol = am.getStreamVolume(AudioManager.STREAM_MUSIC);
                 touchRang = Math.min(screenHeight,screenWidth);//screenHeight
                 handler.removeMessages(HIDE_MEDIACONTROLLER);
@@ -486,24 +505,34 @@ public class VitamioVideoPlayerActivity extends AppCompatActivity implements Vie
                 float endY = event.getY();
                 //3.计算滑动的距离
                 float distanceY = dowY - endY;
-                //原理：在屏幕滑动的距离： 滑动的总距离 = 要改变的声音： 最大声音
-                //要改变的声音 = （在屏幕滑动的距离/ 滑动的总距离）*最大声音;
-                float delta = (distanceY/touchRang)*maxVoice;
+                if (dowX > screenWidth / 2) {
+                    //右边
+                    //在这里处理音量
+                    //原理：在屏幕滑动的距离： 滑动的总距离 = 要改变的声音： 最大声音
+                    //要改变的声音 = （在屏幕滑动的距离/ 滑动的总距离）*最大声音;
+                    float delta = (distanceY/touchRang)*maxVoice;
 
 
-                if(delta != 0){
-                    //最终声音 = 原来的+ 要改变的声音
-                    int mVoice = (int) Math.min(Math.max(mVol+delta,0),maxVoice);
-                    //0~15
+                    if(delta != 0){
+                        //最终声音 = 原来的+ 要改变的声音
+                        int mVoice = (int) Math.min(Math.max(mVol+delta,0),maxVoice);
+                        //0~15
+                        updateVoiceProgress(mVoice);
+                    }
 
-                    updateVoiceProgress(mVoice);
+                } else {
+                    //屏幕左半部分上滑，亮度变大，下滑，亮度变小
+                    final double FLING_MIN_DISTANCE = 0.5;
+                    final double FLING_MIN_VELOCITY = 0.5;
+                    if (distanceY > FLING_MIN_DISTANCE && Math.abs(distanceY) > FLING_MIN_VELOCITY) {
+                        setBrightness(10);
+                    }
+                    if (distanceY < FLING_MIN_DISTANCE && Math.abs(distanceY) > FLING_MIN_VELOCITY) {
+                        setBrightness(-10);
+                    }
                 }
-
-
                 //注意不要重新赋值
 //                dowY = event.getY();
-
-
                 break;
             case MotionEvent.ACTION_UP:
                 handler.sendEmptyMessageDelayed(HIDE_MEDIACONTROLLER,4000);
