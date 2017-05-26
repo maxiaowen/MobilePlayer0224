d272010c4c44d270d620318c3276ae33b66ba76c
maxiaowen <332168460@qq.com>
2017-05-26 14:56:59 +0800

    绘制歌词

--- app/src/main/java/com/atguigu/mobileplayer0224/view/LyricShowView.java
+++ app/src/main/java/com/atguigu/mobileplayer0224/view/LyricShowView.java
@@ -17,10 +17,18 @@ import java.util.ArrayList;
 
 public class LyricShowView extends TextView {
 
-    private Paint paint;
+    private Paint paintGreen;
+    private Paint paintWhite;
 
     private int width;
     private int heigth;
+
+    /**
+     * 表示的是歌词列表中的哪一句
+     */
+    private int index = 0;
+    //每句歌词所占的高度
+    private float textHeight = 20;
 
     private ArrayList<Lyric> lyrics;
 
@@ -31,15 +39,25 @@ public class LyricShowView extends TextView {
     }
 
     private void initView() {
-        paint = new Paint();
+        paintGreen = new Paint();
         //设置画笔颜色
-        paint.setColor(Color.GREEN);
+        paintGreen.setColor(Color.GREEN);
         //设置抗锯齿
-        paint.setAntiAlias(true);
+        paintGreen.setAntiAlias(true);
         //设置文字大小
-        paint.setTextSize(16);
+        paintGreen.setTextSize(16);
         //设置居中
-        paint.setTextAlign(Paint.Align.CENTER);
+        paintGreen.setTextAlign(Paint.Align.CENTER);
+
+        paintWhite = new Paint();
+        //设置画笔颜色
+        paintWhite.setColor(Color.WHITE);
+        //设置抗锯齿
+        paintWhite.setAntiAlias(true);
+        //设置文字大小
+        paintWhite.setTextSize(16);
+        //设置居中
+        paintWhite.setTextAlign(Paint.Align.CENTER);
 
         //准备歌词
         lyrics = new ArrayList<>();
@@ -70,6 +88,46 @@ public class LyricShowView extends TextView {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawText("没有找到歌词...",width/2,heigth/2,paint);
+        
+        if(lyrics != null && lyrics.size() > 0) {
+            //有歌词
+            //当前句-中心的哪一句
+            String currentContent = lyrics.get(index).getContent();
+            canvas.drawText(currentContent,width/2,heigth/2,paintGreen);
+
+            //得到中间句的坐标
+            float tempY = heigth/2;
+            //绘制前面部分
+            for (int i = index - 1; i >= 0; i--) {
+
+                //得到前一部分多月的歌词内容
+                String preContent = lyrics.get(i).getContent();
+
+                tempY = tempY - textHeight;
+                if (tempY < 0) {
+                    break;
+                }
+
+                //绘制内容
+                canvas.drawText(preContent, width / 2, tempY, paintWhite);
+            }
+
+            tempY = heigth / 2;
+            //绘制后面部分
+            for (int i = index + 1; i < lyrics.size(); i++) {
+                //得到后一部分多月的歌词内容
+                String nextContent = lyrics.get(i).getContent();
+
+                tempY = tempY + textHeight;
+                if (tempY > heigth) {
+                    break;
+                }
+                //绘制内容
+                canvas.drawText(nextContent, width / 2, tempY, paintWhite);
+            }
+
+        }else {
+            canvas.drawText("没有找到歌词...",width/2,heigth/2,paintGreen);
+        }
     }
 }
