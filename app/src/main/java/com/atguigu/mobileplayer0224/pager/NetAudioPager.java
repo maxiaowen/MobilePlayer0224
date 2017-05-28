2c99613cf5700669d3e75c1f902acaa6f2cd4541
maxiaowen <332168460@qq.com>
2017-05-28 14:33:14 +0800

    设置图片和GIF的点击事件

--- app/src/main/java/com/atguigu/mobileplayer0224/pager/NetAudioPager.java
+++ app/src/main/java/com/atguigu/mobileplayer0224/pager/NetAudioPager.java
@@ -1,12 +1,15 @@
 package com.atguigu.mobileplayer0224.pager;
 
+import android.content.Intent;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.atguigu.mobileplayer0224.R;
+import com.atguigu.mobileplayer0224.activity.ShowImageAndGifActivity;
 import com.atguigu.mobileplayer0224.adapter.NetAudioFragmentAdapter;
 import com.atguigu.mobileplayer0224.domain.MediaItem;
 import com.atguigu.mobileplayer0224.domain.NetAudioBean;
@@ -55,6 +58,30 @@ public class NetAudioPager extends BaseFragment {
         View view = View.inflate(context, R.layout.fragment_net_audio, null);
 
         ButterKnife.inject(this, view);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+                NetAudioBean.ListBean listEntity = datas.get(position);
+                if(listEntity !=null ){
+                    //3.传递视频列表
+                    Intent intent = new Intent(context,ShowImageAndGifActivity.class);
+                    if(listEntity.getType().equals("gif")){
+                        String url = listEntity.getGif().getImages().get(0);
+                        intent.putExtra("url",url);
+                        context.startActivity(intent);
+                    }else if(listEntity.getType().equals("image")){
+                        String url = listEntity.getImage().getThumbnail_small().get(0);
+                        intent.putExtra("url",url);
+                        context.startActivity(intent);
+                    }
+                }
+
+
+            }
+        });
 
         return view;
     }
